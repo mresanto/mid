a772a9d9db9dde9479147767cf48f43ee2ceebd1
mresanto <micaelrodrigues504@gmail.com>
2026-06-17 11:57:10 -0300

    [feat] query last

--- src/cli/commands/query/mod.rs
+++ src/cli/commands/query/mod.rs
@@ -3,7 +3,10 @@ use clap::Subcommand;
 
 #[derive(Subcommand)]
 pub enum QueryCommands {
-    Last {},
+    Last {
+        #[arg(short, long)]
+        output_format: Option<QueryOutputFormat>,
+    },
     History {},
 }
 
@@ -13,7 +16,32 @@ pub async fn handle_query_command(
     output_format: &Option<QueryOutputFormat>,
 ) -> () {
     match command {
-        Some(QueryCommands::Last {}) => {}
+        Some(QueryCommands::Last { output_format }) => {
+            let file_path_history = core::globals::get_global_history_file_path();
+            let last_request = core::history::read_history(file_path_history);
+
+            match last_request {
+                Ok(history) => match history.requests.last() {
+                    Some(last) => {
+                        let res = core::query::handle_query_command(
+                            last.query.clone(),
+                            output_format
+                                .as_ref()
+                                .unwrap_or(&QueryOutputFormat::Table)
+                                .clone(),
+                        )
+                        .await;
+
+                        match res {
+                            Ok(_) => {}
+                            Err(e) => eprintln!("Failed to execute query command: {e}"),
+                        }
+                    }
+                    _ => println!("No history found"),
+                },
+                Err(e) => println!("No history found {}", e),
+            }
+        }
         _ => {
             let Some(query) = query else {
                 eprintln!("Failed to execute query command: query is required");
